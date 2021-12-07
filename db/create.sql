b64c05ada33e63ab4ad11617ee9214b86815a7cd
emmauskatty <113344@emmauscollege.nl>
2021-12-07 19:42:57 +0000

    opzet database versie 7 dec

--- db/create.sql
+++ db/create.sql
@@ -8,35 +8,42 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2),
-  type_of_phone_id TEXT,
-  colour_id INTEGER,
-  type_of_phonecase_id INTEGER
+  price NUMERIC(10, 2)
+  --type_of_phone_id TEXT,
+  --colour_id INTEGER,
+  --type_of_phonecase_id INTEGER
 );
 
-DROP TABLE IF EXISTS type_of_phonecase;
+/*DROP TABLE IF EXISTS type_of_phonecase;
 CREATE TABLE type_of_phonecase (
   type_of_phonecase_id INTEGER PRIMARY KEY,
   type_of_phonecase_name TEXT
-)
+)*/
 
-DROP TABLE IF EXISTS type_of_phone ;
-CREATE TABLE soort (
-  type_of_phone_id INTEGER PRIMARY KEY,
+DROP TABLE IF EXISTS type_of_phone;
+CREATE TABLE type_of_phone (
+  type_of_phone_id INTEGER PRIMARY KEY AUTOINCREMENT,
   type_of_phone_name TEXT
 );
 
-DROP TABLE IF EXISTS type_of_phone.type_of_phonecase;
+DROP TABLE IF EXISTS products.type_of_phone;
+CREATE TABLE products.type_of_phone (
+ products.type_of_phone_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ products_id INTEGER,
+ type_of_phone_id INTEGER
+);
+
+/*DROP TABLE IF EXISTS type_of_phone.type_of_phonecase;
 CREATE TABLE type_of_phone.type_of_phonecase (
   type_of_phone.type_of_phonecase_id INTEGER PRIMARY KEY,
   type_of_phone_id INTEGER,
   type_of_phonecase_id INTEGER
-)
+)*/
 
 DROP TABLE IF EXISTS colour;
 CREATE TABLE colour (
  colour_id INTEGER PRIMARY KEY AUTOINCREMENT,
- colour_naam TEXT
+ colour_name TEXT
 );
 
 DROP TABLE IF EXISTS product.colour;
@@ -66,10 +73,48 @@ insert into products (name, description, code, price) values ('Velvet Goldmine',
 
 Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 14);
 
-insert into colour (colour_naam) values ('Pink');
-insert into colour (colour_naam) values ('Green');
-insert into colour (colour_naam) values ('Blue');
-insert into colour (colour_naam) values ('Purple');
+insert into type_of_phone (type_of_phone_name) values ('Samsung Galaxy A52s');
+insert into type_of_phone (type_of_phone_name) values ('Samsung Galaxy S20');
+insert into type_of_phone (type_of_phone_name) values ('Apple iPhone SE');
+insert into type_of_phone (type_of_phone_name) values ('Apple iPhone 11');
+insert into type_of_phone (type_of_phone_name) values ('Apple iPhone 12');
+insert into type_of_phone (type_of_phone_name) values ('Apple iPhone 13')
+insert into type_of_phone (type_of_phone_name) values ('Xiaomi Poco X3 Pro ');
+insert into type_of_phone (type_of_phone_name) values ('Huawei P30');
 
-insert into product.colour (type_of_phone_id, type_of_phonecase_id);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (1, 1);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (1, 2);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (1, 3);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (1, 4);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (1, 5);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (1, 6);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (1, 7);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (1, 8);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (2, 1);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (2, 2);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (2, 3);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (2, 4);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (2, 5);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (2, 6);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (2, 7);
+insert into products.type_of_phone (products_id, type_of_phone_id) values (2, 8);
+
+insert into colour (colour_name) values ('Pink');
+insert into colour (colour_name) values ('Green');
+insert into colour (colour_name) values ('Blue');
+insert into colour (colour_name) values ('Purple');
+insert into colour (colour_name) values ('Yellow');
+
+insert into product.colour (product_id, colour_id) values (1, 1);
+insert into product.colour (product_id, colour_ id) values (1, 2);
+insert into product.colour (product_id, colour_ id) values (1, 3);
+insert into product.colour (product_id, colour_ id) values (1, 4);
+insert into product.colour (product_id, colour_ id) values (1, 5);
+insert into product.colour (product_id, colour_ id) values (2, 1);
+insert into product.colour (product_id, colour_ id) values (2, 2);
+insert into product.colour (product_id, colour_ id) values (2, 3);
+insert into product.colour (product_id, colour_ id) values (2, 4);
+insert into product.colour (product_id, colour_ id) values (2, 5);
+
+
 
